caf1961fcdf1495d739a01274af51db03392c865
poseidon2022 <kidusm82@gmail.com>
2024-10-02 02:56:27 +0300

    cors: functionality for cross site

--- delivery/main.go
+++ delivery/main.go
@@ -4,10 +4,21 @@ import (
 	routers "mereb_go/delivery/router"
 	domain "mereb_go/domain"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	router := gin.Default()
+
+	corsConfig := cors.Config{
+        AllowAllOrigins: true,
+        AllowMethods:    []string{"GET", "POST", "PUT", "DELETE"}, 
+        AllowHeaders:   []string{"Origin", "Content-Type", "Accept"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true, 
+    }
+
+    router.Use(cors.New(corsConfig))
 	routers.PersonRoutes(router, &domain.InMemory)
 	router.Run("localhost:3000")
 }
