5e45439ff5312933652caebd1d2fe7ad7092b3c0
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-10-02 14:34:39 +0300

    controller implementation

---
+++ controllers/controllers.go
@@ -0,0 +1,84 @@
+package controllers
+
+import (
+	"github.com/abe16s/GoCrudChallange/domain"
+	"github.com/abe16s/GoCrudChallange/usecases"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+type Controller struct {
+	service usecases.IService
+}
+
+func NewController(service usecases.IService) *Controller {
+	return &Controller{service: service}
+}
+
+func (c *Controller) CreatePerson(ctx *gin.Context) {
+	var person domain.Person
+	if err := ctx.ShouldBindJSON(&person); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := c.service.CreatePerson(person.Name, person.Age, person.Hobbies); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusCreated, person)
+}
+
+func (c *Controller) GetAllPersons(ctx *gin.Context) {
+	persons, err := c.service.GetAllPersons()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, persons)
+}
+
+func (c *Controller) GetPersonById(ctx *gin.Context) {
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	person, err := c.service.GetPersonById(id)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, person)
+}
+
+func (c *Controller) UpdatePerson(ctx *gin.Context) {
+	var person domain.Person
+	if err := ctx.ShouldBindJSON(&person); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := c.service.UpdatePerson(id, person.Name, person.Age, person.Hobbies); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "person updated"})
+}
+
+func (c *Controller) DeletePerson(ctx *gin.Context) {
+	id, err := uuid.Parse(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := c.service.DeletePerson(id); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "person deleted"})
+}
