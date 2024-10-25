3646dda25a4ca142fa1047b6f337903348859214
mahider <95926290+Mahider-T@users.noreply.github.com>
2024-10-25 22:51:42 +0300

    Implement database and handler in adapters

--- internal/adapters/db/user.db.go
+++ internal/adapters/db/user.db.go
@@ -1,1 +1,51 @@
 package db
+
+import (
+	"github.com/Mahider-T/GoCrudChallange/internal/application/core/domain"
+	"github.com/google/uuid"
+)
+
+type UserRepo struct {
+	store map[string]domain.User
+}
+
+func NewUserRepo(store map[string]domain.User) *UserRepo {
+	return &UserRepo{
+		store: store,
+	}
+}
+func (ur *UserRepo) Create(user domain.User) (*domain.User, error) {
+	id := uuid.New().String()
+	user.Id = id
+	ur.store[id] = user
+	// usr := ur.store[id] //Task
+	usr, err := ur.ReadOne(id)
+	if err != nil {
+		return nil, err
+	}
+	return usr, nil
+}
+
+func (ur *UserRepo) ReadOne(id string) (*domain.User, error) {
+	usr := ur.store[id]
+	return &usr, nil
+}
+func (ur *UserRepo) ReadAll() ([]*domain.User, error) {
+	usrs := []*domain.User{}
+
+	for _, value := range ur.store {
+		usrs = append(usrs, &value)
+	}
+	return usrs, nil
+}
+
+func (ur *UserRepo) Update(id string, user domain.User) (*domain.User, error) {
+	_, err := ur.ReadOne(id)
+
+	if err != nil {
+		return nil, err
+	}
+	ur.store[id] = user
+	usr := ur.store[id]
+	return &usr, nil
+}
--- internal/adapters/handlers/user.handler.go
+++ internal/adapters/handlers/user.handler.go
@@ -1,1 +1,43 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Mahider-T/GoCrudChallange/internal/application/core/domain"
+	"github.com/Mahider-T/GoCrudChallange/internal/ports/apiport"
+)
+
+type UserHandler struct {
+	UserApi apiport.UserApiPort
+}
+
+func NewUserHandler(userApi apiport.UserApiPort) *UserHandler {
+	return &UserHandler{
+		UserApi: userApi,
+	}
+}
+
+func (uh *UserHandler) UserCreatePost(w http.ResponseWriter, r *http.Request) {
+	var user domain.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	createdUser, err := uh.UserApi.CreateUser(user)
+	if err != nil {
+		http.Error(w, "Could not create user", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	err = json.NewEncoder(w).Encode(createdUser)
+	if err != nil {
+		http.Error(w, "Error encoding response", http.StatusInternalServerError)
+		return
+	}
+}
