f4e6ea8ccfffb37368b03a451524bc85fc033bfe
mahider <95926290+Mahider-T@users.noreply.github.com>
2024-10-25 22:47:56 +0300

    add api and db ports

--- internal/ports/apiport/user.apiPort.go
+++ internal/ports/apiport/user.apiPort.go
@@ -1,1 +1,10 @@
 package apiport
+
+import "github.com/Mahider-T/GoCrudChallange/internal/application/core/domain"
+
+type UserApiPort interface {
+	CreateUser(user domain.User) (*domain.User, error)
+	GetUserByID(id string) (*domain.User, error)
+	GetAllUsers() ([]*domain.User, error)
+	UpdateUser(id string, user domain.User) (*domain.User, error)
+}
--- internal/ports/dbport/user.dbPort.go
+++ internal/ports/dbport/user.dbPort.go
@@ -1,1 +1,10 @@
 package dbport
+
+import "github.com/Mahider-T/GoCrudChallange/internal/application/core/domain"
+
+type UserDbPort interface {
+	Create(user domain.User) (*domain.User, error)
+	ReadOne(id string) (*domain.User, error)
+	ReadAll() ([]*domain.User, error)
+	Update(id string, user domain.User) (*domain.User, error)
+}
