2b0a0d1bb0f78bb95c9a7372f40d972f9e2cdf50
mahider <95926290+Mahider-T@users.noreply.github.com>
2024-10-25 22:52:55 +0300

    configure app entrypoint with di

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Mahider-T/GoCrudChallange/internal/adapters/db"
+	"github.com/Mahider-T/GoCrudChallange/internal/adapters/handlers"
+	"github.com/Mahider-T/GoCrudChallange/internal/application/core/api"
+	"github.com/Mahider-T/GoCrudChallange/internal/application/core/domain"
+	"github.com/Mahider-T/GoCrudChallange/internal/ports/apiport"
+	// "github.com/Mahider-T/GoCrudChallange/internal/ports/dbport"
+)
+
+type Application struct {
+	// userRepo    dbport.UserDbPort
+	userService apiport.UserApiPort
+}
+
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	var storage = make(map[string]domain.User)
+
+	app := &Application{
+		// userRepo:    db.NewUserRepo(storage),
+		userService: api.NewUserApi(db.NewUserRepo(storage)),
+	}
+
+	userHandler := handlers.NewUserHandler(app.userService)
+
+	mux.HandleFunc("POST /users", userHandler.UserCreatePost)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Listening on port:", port)
+	http.ListenAndServe(":"+port, enableCors(mux))
+
+}
