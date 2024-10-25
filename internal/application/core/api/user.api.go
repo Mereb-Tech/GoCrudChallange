bc624eaf60213e3a95b3882762c20a0c4d29ac12
mahider <95926290+Mahider-T@users.noreply.github.com>
2024-10-25 22:49:24 +0300

    implement user api(service)

--- internal/application/core/api/user.api.go
+++ internal/application/core/api/user.api.go
@@ -1,1 +1,33 @@
 package api
+
+import (
+	"github.com/Mahider-T/GoCrudChallange/internal/application/core/domain"
+	"github.com/Mahider-T/GoCrudChallange/internal/ports/dbport"
+)
+
+type UserApi struct {
+	UserDbPort dbport.UserDbPort
+}
+
+func NewUserApi(userDbPort dbport.UserDbPort) *UserApi {
+	return &UserApi{
+		UserDbPort: userDbPort,
+	}
+}
+
+func (ua *UserApi) CreateUser(user domain.User) (*domain.User, error) {
+	return ua.UserDbPort.Create(user)
+
+}
+
+func (ua *UserApi) GetUserByID(id string) (*domain.User, error) {
+	return ua.UserDbPort.ReadOne(id)
+}
+
+func (ua *UserApi) GetAllUsers() ([]*domain.User, error) {
+	return ua.UserDbPort.ReadAll()
+}
+
+func (ua *UserApi) UpdateUser(id string, user domain.User) (*domain.User, error) {
+	return ua.UserDbPort.Update(id, user)
+}
